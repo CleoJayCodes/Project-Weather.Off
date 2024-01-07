76ad8f4b8d912807d08669d84401616256ece810
CleoJayCodes <jarnaeblackett@gmail.com>
2024-01-07 22:09:08 +0000

    Added basic search engine

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+function citySearchSubmit(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#current-country");
+  cityElement.innerHTML = searchInput.value;
+}
+
 let searchFormElement = document.querySelector("#search-form");
+searchFormElement.addEventListener("submit", citySearchSubmit);
